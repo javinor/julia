3f4fd8e05e27b238064b64f11d1e2d2354d23bda
javinor <javinor@gmail.com>
2017-04-08 14:54:23 +0300

    remove all webgl-utils unused functions

--- src/mandelbrot.js
+++ src/mandelbrot.js
@@ -9,37 +9,15 @@ const mandelbrotShaders = require('./mandelbrotShaders')
 const vertexShaderSource = mandelbrotShaders.vertexShaderSource
 const fragmentShaderSource = mandelbrotShaders.fragmentShaderSource
 
-function createShader(gl, type, source) {
-  const shader = gl.createShader(type)
-  gl.shaderSource(shader, source)
-  gl.compileShader(shader)
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
-  if (success) return shader
-
-  console.log(gl.getShaderInfoLog(shader))
-  gl.deleteShader(shader)
-}
-
-function createProgram(gl, vertexShader, fragmentShader) {
-  const program = gl.createProgram()
-  gl.attachShader(program, vertexShader)
-  gl.attachShader(program, fragmentShader)
-  gl.linkProgram(program)
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS)
-  if (success) return program
-
-  console.log(gl.getProgramInfoLog(program))
-  gl.deleteProgram(program)
-}
+const vertexShader = webglUtils.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
+const fragmentShader = webglUtils.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
+const program = webglUtils.createProgram(gl, vertexShader, fragmentShader)
 
 
-const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
-const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
-const program = createProgram(gl, vertexShader, fragmentShader)
+const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
+const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution')
+const colorUniformLocation = gl.getUniformLocation(program, 'u_color')
 
-const positionAttributeLocation = gl.getAttribLocation(program, "a_position")
-const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
-const colorUniformLocation = gl.getUniformLocation(program, "u_color");
 
 const positionBuffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
