d7e535c02d21c0101b15d0b3e1cf9238ca4899c6
javinor <javinor@gmail.com>
2017-04-15 14:48:40 +0300

    smooth julia

--- src/julia.js
+++ src/julia.js
@@ -12,6 +12,11 @@ const fragmentShader = webglUtils.createShader(gl, gl.FRAGMENT_SHADER, fragmentS
 const program = webglUtils.createProgram(gl, vertexShader, fragmentShader)
 
 const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
+const cUniformLocation = gl.getUniformLocation(program, 'c')
+const bottomUniformLocation = gl.getUniformLocation(program, 'bottom')
+const leftUniformLocation = gl.getUniformLocation(program, 'left')
+const heightUniformLocation = gl.getUniformLocation(program, 'height')
+const widthUniformLocation = gl.getUniformLocation(program, 'width')
 
 const positionBuffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
@@ -20,14 +25,13 @@ var positions = [-1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, -1]
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
 // RENDERING CODE
-function drawMandelbrot() {
+function drawJulia() {
   webglUtils.resizeCanvasToDisplaySize(gl.canvas)
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
   gl.clearColor(0, 0, 0, 0)
   gl.clear(gl.COLOR_BUFFER_BIT)
 
-  // Tell it to use our program (pair of shaders)
   gl.useProgram(program)
 
   gl.enableVertexAttribArray(positionAttributeLocation)
@@ -40,7 +44,14 @@ function drawMandelbrot() {
   var offset = 0        // start at the beginning of the buffer
   gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
 
+// -0.828 - 0.180i
+  gl.uniform2f(cUniformLocation, -0.828, -0.180);
+  gl.uniform1f(bottomUniformLocation, -2);
+  gl.uniform1f(leftUniformLocation, -2);
+  gl.uniform1f(heightUniformLocation, 4);
+  gl.uniform1f(widthUniformLocation, 4);
+
   gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2)
 }
 
-drawMandelbrot()
+drawJulia()
--- src/juliaShaders.js
+++ src/juliaShaders.js
@@ -2,47 +2,60 @@
 
 const vertexShaderSource = `
   attribute vec2 a_position;
+
+  uniform float bottom;
+  uniform float left;
+  uniform float height;
+  uniform float width;
+
   varying vec2 v_position;
 
   void main() {
     gl_Position = vec4(a_position, 0, 1);
 
-    float x = a_position.x;
-    float y = a_position.y;
+    float x = ((a_position.x + 1.0) / 2.0) * width + left;
+    float y = ((a_position.y + 1.0) / 2.0) * height + bottom;
+
     v_position = vec2(x, y);
   }
 `
 const fragmentShaderSource = `
-  precision mediump float;
+  precision highp float;
 
+  uniform vec2 c;
   varying vec2 v_position;
 
   void main() {
-    vec4 white = vec4(1.0, 1.0, 1.0, 1.0);
-    vec4 black = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 black = vec4(0, 0, 0, 1);
+    vec4 color = vec4(0, 0, 0, 1);
 
-    float norm;
+    float iter = 0.0;
     float x = v_position.x;
     float y = v_position.y;
     float tempX;
     float tempY;
 
     for (int i = 0; i < 4000; i++) {
+      iter += 1.0;
       tempX = x * x - y * y;
       tempY = 2.0 * x * y;
 
-      x = tempX - 0.71;
-      y = tempY - 0.2;
+      x = tempX + c.x;
+      y = tempY + c.y;
 
-      norm = x * x + y * y;
-
-      if (norm > 4.0) break;
+      if (length(vec2(x,y)) > 4.0) break;
     }
 
-    if (norm > 4.0) {
-      gl_FragColor = black;
+    if (length(vec2(x,y)) > 4.0) {
+      iter -= log2(log2(length(vec2(x,y))));
+      color.r = 0.5 + 0.50 * sin(iter);
+      color.g = 0.5 + 0.25 * sin(iter);
+      color.b = 0.5 - 0.25 * cos(iter);
+      color.a = 1.0;
+
+      gl_FragColor = color;
     } else {
-      gl_FragColor = white;
+      gl_FragColor = black;
     }
   }
 `
