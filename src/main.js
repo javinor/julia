520d171057522d803ca09e2392392cd898787359
javinor <javinor@gmail.com>
2017-04-16 13:39:18 +0300

    enable panning (still has slight drift)

--- src/main.js
+++ src/main.js
@@ -6,4 +6,22 @@ import Julia from './julia'
 
 const canvas = document.getElementById('julia')
 const julia = new Julia({canvas})
-julia.render()
+
+const width = canvas.width
+const renderOptions = {
+  complexCenter: [0, 0],
+  constant: [-0.828, -0.180],
+  xLength: 4
+}
+julia.render(renderOptions)
+
+const mouseDownCoords = {}
+const rerenderJulia = (e) => {
+  const pixelToDistanceRatio = renderOptions.xLength / width
+  renderOptions.complexCenter[0] -= e.movementX * pixelToDistanceRatio
+  renderOptions.complexCenter[1] += e.movementY * pixelToDistanceRatio
+  window.requestAnimationFrame(julia.render.bind(julia, renderOptions))
+}
+
+canvas.addEventListener('mousedown', () => canvas.addEventListener('mousemove', rerenderJulia))
+canvas.addEventListener('mouseup', () => canvas.removeEventListener('mousemove', rerenderJulia))
