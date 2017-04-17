037c9a446283b2f5f214dca79bcfa82369e550e8
javinor <javinor@gmail.com>
2017-04-17 17:50:34 +0300

    add keyboard panning

--- src/main.js
+++ src/main.js
@@ -16,25 +16,29 @@ const debouncedRender = new Debouncer(() => julia.render())
 window.onresize = () => debouncedRender.exec()
 
 let prevMouseCoords;
-const debouncedPan = new Debouncer((e) => {
+const debouncedMousePan = new Debouncer((e) => {
   const pixelDeltaX = e.clientX - prevMouseCoords.x
   const pixelDeltaY = e.clientY - prevMouseCoords.y
   julia.pan(pixelDeltaX, pixelDeltaY)
   prevMouseCoords.x = e.clientX
   prevMouseCoords.y = e.clientY
 })
-const mouseMoveListener = (e) => debouncedPan.exec(e)
-
+const mouseMoveListener = (e) => debouncedMousePan.exec(e)
 canvas.addEventListener('mouseup', () => canvas.removeEventListener('mousemove', mouseMoveListener))
 canvas.addEventListener('mousedown', (e) => {
   prevMouseCoords = { x: e.clientX, y: e.clientY }
   canvas.addEventListener('mousemove', mouseMoveListener)
 })
 
-const debouncedZoom = new Debouncer((multiplier) => julia.zoom(multiplier))
+const debouncedKeyboardZoom = new Debouncer((multiplier) => julia.zoom(multiplier))
+const debouncedKeyboardPan = new Debouncer((dx, dy) => julia.pan(dx, dy))
 document.addEventListener('keydown', (e) => {
   switch (e.key) {
-    case 'a': debouncedZoom.exec(0.9); break;
-    case 'z': debouncedZoom.exec(1.1); break;
+    case 'a': debouncedKeyboardZoom.exec(0.9); break;
+    case 'z': debouncedKeyboardZoom.exec(1.1); break;
+    case 'ArrowUp': debouncedKeyboardPan.exec(0, 5); break;
+    case 'ArrowRight': debouncedKeyboardPan.exec(-5, 0); break;
+    case 'ArrowDown': debouncedKeyboardPan.exec(0, -5); break;
+    case 'ArrowLeft': debouncedKeyboardPan.exec(5, 0); break;
   }
 })
