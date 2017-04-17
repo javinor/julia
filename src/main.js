2cbba34d93806a41ab201740cdd87102fdf9fd53
javinor <javinor@gmail.com>
2017-04-17 18:52:19 +0300

    change constant with mouse click

--- src/main.js
+++ src/main.js
@@ -20,25 +20,11 @@ const debouncedChangeConstant = new Debouncer((pixelPoint) => {
   julia.changeConstant(pixelPoint)
   juliaConstantEl.innerText = JSON.stringify(julia.constant, (key, value) => isNaN(value) ? value : value.toFixed(3))
 })
-canvas.addEventListener('dblclick', (e) => {
-  debouncedChangeConstant.exec({
-    x: e.clientX,
-    y: e.clientY
-  })
-})
 
-let prevMouseCoords
-const debouncedMousePan = new Debouncer((e) => {
-  const pixelDeltaX = e.clientX - prevMouseCoords.x
-  const pixelDeltaY = e.clientY - prevMouseCoords.y
-  julia.pan(pixelDeltaX, pixelDeltaY)
-  prevMouseCoords.x = e.clientX
-  prevMouseCoords.y = e.clientY
-})
-const mouseMoveListener = (e) => debouncedMousePan.exec(e)
+const mouseMoveListener = (e) => debouncedChangeConstant.exec({x: e.clientX, y: e.clientY})
 canvas.addEventListener('mouseup', () => canvas.removeEventListener('mousemove', mouseMoveListener))
 canvas.addEventListener('mousedown', (e) => {
-  prevMouseCoords = { x: e.clientX, y: e.clientY }
+  debouncedChangeConstant.exec({x: e.clientX, y: e.clientY})
   canvas.addEventListener('mousemove', mouseMoveListener)
 })
 
