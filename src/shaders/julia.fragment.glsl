599e0c68a42e9221b453fe4599e00cb1c00ae176
javinor <javinor@gmail.com>
2017-04-18 03:33:41 +0300

    trying new color function

--- src/shaders/julia.fragment.glsl
+++ src/shaders/julia.fragment.glsl
@@ -5,7 +5,6 @@ varying vec2 complexPoint;
 
 void main() {
   vec4 black = vec4(0, 0, 0, 1);
-  vec4 color = vec4(0, 0, 0, 1);
 
   float iter = 0.0;
   float x = complexPoint.x;
@@ -25,17 +24,20 @@ void main() {
   }
 
   if (length(vec2(x,y)) > 4.0) {
+    // http://www.iquilezles.org/www/articles/palettes/palettes.htm
+    //vec3 a = vec3(0.5, 0.5, 0.5);
+    //vec3 b = vec3(0.5, 0.5, 0.5);
+    //vec3 c = vec3(1.0,1.0,.5);
+    //vec3 d = vec3(0.90, 0.10, 0.20);
+
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(0.3, 0.5, 0.7);
+    vec3 d = vec3(0.4, 0.15, 0.20);
+
     iter -= log2(log2(length(vec2(x,y))));
-    // color.r = 0.5 + 0.50 * sin(iter);
-    // color.g = 0.5 + 0.25 * sin(iter);
-    // color.b = 0.5 - 0.25 * cos(iter);
 
-    color.r = 0.5 + 0.5 * sin(iter);
-    color.g = 0.5 + 0.5 * cos(iter);
-    color.b = 0.5 - 0.5 * sin(iter);
-    color.a = 1.0;
-
-    gl_FragColor = color;
+    gl_FragColor = vec4(a + b * cos(6.28318 * (c * iter + d)), 1);
   } else {
     gl_FragColor = black;
   }
